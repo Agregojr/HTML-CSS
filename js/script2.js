96bbe34679258fabf8faf59dc2a81585d3483260
Agregojr <92557170+Agregojr@users.noreply.github.com>
2022-09-26 11:33:18 -0300

    Atividade hands on iniciada

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-document.write("<h1>Executando script2.js</h1> ");
-document.write("Esse script está em um arquivo externo");
-document.write("Referencia no fim do body do html");
-
-function mudarCor(novaCor) {
-    var x = document. getElementById('demo');
-    x.style.color = novaCor;
-}
